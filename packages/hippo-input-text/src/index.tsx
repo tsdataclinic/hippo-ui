c514b30e824490ac8c563d95a554549c90736f37
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-09-16 13:58:39 -0400

    Added react-aria textfield

--- packages/hippo-input-text/src/index.tsx
+++ packages/hippo-input-text/src/index.tsx
@@ -1,23 +1,28 @@
 import * as React from 'react';
+import { useTextField, type AriaTextFieldOptions } from '@react-aria/textfield';
+import { useComposedRefs } from '@hippo/utils';
 
-type HTMLInputProps = React.ComponentPropsWithoutRef<'input'>;
 type HTMLInputRef = React.ElementRef<'input'>;
 
-export interface InputTextProps extends HTMLInputProps {
-  value: string;
+export interface InputTextProps extends AriaTextFieldOptions<'input'> {
+  /**
+   * The value to set for the InputText. If no value is provided, then the
+   * InputText is treated as uncontrolled.
+   */
+  value?: string;
 }
 
+/**
+ * An InputText component.
+ * Is uncontrolled by default until a `value` is passed.
+ */
 export const InputText = React.forwardRef<HTMLInputRef, InputTextProps>(
   (props: InputTextProps, forwardedRef: React.ForwardedRef<HTMLInputRef>) => {
-    const { value, ...passThroughProps } = props;
-    return (
-      <input
-        ref={forwardedRef}
-        type="text"
-        value={value}
-        {...passThroughProps}
-      />
-    );
+    const inputRef = React.useRef<HTMLInputElement | null>(null);
+    const composedRef = useComposedRefs(forwardedRef, inputRef);
+    const { inputProps } = useTextField(props, composedRef);
+    const { value, ...passThroughInputProps } = inputProps;
+    return <input {...passThroughInputProps} ref={forwardedRef} type="text" />;
   },
 );
 
